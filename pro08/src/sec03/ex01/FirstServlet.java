d2625bbd38001c44eb97eca43e3b7edfba89775a
LEE Hyeon Seung <wila@tistory.com>
2020-05-15 11:44:43 +0900

    ServletContext

---
+++ pro08/src/sec03/ex01/FirstServlet.java
@@ -0,0 +1,38 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+//@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    /**
+     * Default constructor. 
+     */
+    public FirstServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher dispatch = request.getRequestDispatcher("second?name=lee");
+		dispatch.forward(request,  response);
+	}
+
+}
