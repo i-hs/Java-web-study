e4b878dced3fdc5a2284c01a8700b5cd6442cc92
LEE Hyeon Seung <wila@tistory.com>
2020-06-10 12:44:39 +0900

    회원 기능 이용해 DI 실습하기

---
+++ pro19/src/com/spring/ex03/MemberTest1.java
@@ -0,0 +1,21 @@
+package com.spring.ex03;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class MemberTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("member.xml"));
+		MemberService service = (MemberService) factory.getBean("memberService");
+		service.listMembers();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
