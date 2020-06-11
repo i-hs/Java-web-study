6383d42b45838940d1476243d2d2aee3b24f3ec2
LEE Hyeon Seung <wila@tistory.com>
2020-06-11 16:42:04 +0900

    요청명과 동일한 JSP 표시하기

--- pro21/src/com/spring/ex02/UserController.java
+++ pro21/src/com/spring/ex02/UserController.java
@@ -12,14 +12,48 @@ public class UserController extends MultiActionController
 								HttpServletResponse response) throws Exception{
 		String userID="";
 		String userPwd="";
+		String viewName = getViewName(request);
 		ModelAndView mav = new ModelAndView();
 		request.setCharacterEncoding("utf-8");
 		userID = request.getParameter("userID");
 		userPwd = request.getParameter("userPwd");
 		mav.addObject("userID", userID);
 		mav.addObject("userPwd", userPwd);
-		mav.setViewName("result");  // 포워딩할 jsp 이름
+//		mav.setViewName("result");  // 포워딩할 jsp 이름
+		mav.setViewName(viewName);
+		System.out.println("viewName: " + viewName);
 		return mav;
+	}
+	
+	private String getViewName(HttpServletRequest request) throws Exception 
+	{
+		String contextPath = request.getContextPath();
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+		if(uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+		
+		int begin=0;
+		if(!((contextPath==null)||("".contentEquals(contextPath)))) {
+			begin = contextPath.length();
+		}
+		
+		int end;
+		if(uri.indexOf(";") != -1) {
+			end = uri.indexOf(";");
+		}else if (uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		}else {
+			end = uri.length();
+		}
+		String fileName = uri.substring(begin, end);
+		if(fileName.indexOf(".") != -1) {
+			fileName = fileName.substring(0, fileName.lastIndexOf("."));
+		}
+		if(fileName.lastIndexOf("/") != -1) {
+			fileName = fileName.substring(fileName.lastIndexOf("/"), fileName.length());
+		}
+		return fileName;
 	}
 	
 	public ModelAndView memberInfo(HttpServletRequest request, 
@@ -34,7 +68,7 @@ public class UserController extends MultiActionController
 		mav.addObject("pwd", pwd);
 		mav.addObject("name", name);
 		mav.addObject("email", email);
-		mav.setViewName("memberInfo");  // 포워딩할 jsp 이름 
+		mav.setViewName("memberInfo2");  // 포워딩할 jsp 이름 
 		return mav;
 		
 	}
